8de1f05a4605784b38aea3bb2940095d0a3c25c9
ljwlc <liujinwei@inspur.com>
2020-02-28 21:26:18 +0800

    add network data

--- redis_info.py
+++ redis_info.py
@@ -134,6 +134,78 @@ class RedisInfo(object):
 
         mem_info = f'MemTotal: {mem / 1024 / 1024 :.2f} GB'
         return mem_info
+    def pustil_disk_io(self):
+        """
+         固定两个盘: 1) 系统盘vda1（挂载点 '/' ) , 数据盘vdd (挂载点'/data'))
+         ***** 默认vdd  对应 lv-redis 逻辑卷 *****
+        :return:
+        """
+
+        disks_info = {}
+        disk_partitions = psutil.disk_partitions()
+        disk_io = psutil.disk_io_counters(perdisk=True)
+
+        for d in disk_partitions:
+            # print('%s %10s %6s %10s' % (d.device, d.mountpoint,  d.fstype, d.opts))
+            di = psutil.disk_usage(d.mountpoint)
+            disk_name = d.device.rsplit('/', 1)[1]
+
+            # redis部署环境中: 将/dev/mapper/vg--redis-lv--redis 对应为 vdd
+            if disk_name.startswith('vg--redis'):
+                disk_name = 'vdd'
+
+            dio = disk_io[disk_name]
+            disks_info[disk_name] = {
+                'disk_utilization': di.percent,
+                'disk_io': (bt_to_kb(dio.read_bytes),
+                            bt_to_kb(dio.write_bytes))
+            }
+
+        # 获取磁盘io平均等待时间 ioawait (单位 ms)
+        disks_ioawait = self.proc_diskstats(list(disks_info.keys()))
+        for disk_name, val in disks_ioawait.items():
+            # ioawait = delta(ruse+wuse)/delta(rio+wio)  单位: ms
+            disks_info[disk_name]['disk_ioawait'] = val    # (rio, ruse, wio, wuse)
+
+        # process monitor info
+        self.process_disk_info(disks_info)
+
+    def process_disk_info(self, new_disks_info):
+        """
+        calculate && update the monitor information of disks
+        :param new_disks_info:
+        :return:
+        """
+        time_delta = self.time_delta    # s <秒>
+
+        for disk_name, new_info in new_disks_info.items():
+
+            if disk_name in self.disk_info:
+                old_info = self.disk_info[disk_name]
+
+                disk_utilization = float_format(new_info['disk_utilization'])    # %
+                # read_kb_ps,       write_kb_ps,
+                read_kb_ps = float_format((new_info['disk_io'][0] -
+                                           old_info['disk_io'][0])/time_delta)
+                write_kb_ps = float_format((new_info['disk_io'][1] -
+                                            old_info['disk_io'][1])/time_delta)
+
+                # ioawait
+                # delta(ruse+wuse)/delta(rio+wio)  单位: ms
+                nd_ioawait = new_info['disk_ioawait']    # set (rio, ruse, wio, wuse)
+                od_dioawait = old_info['disk_ioawait']    # set
+                delta_rwio = float_format(((nd_ioawait[0] - od_dioawait[0]) +
+                                           (nd_ioawait[2] - od_dioawait[2])))   # kb/s
+                delta_rwuse = float_format(((nd_ioawait[1] - od_dioawait[1]) +
+                                            (nd_ioawait[3] - od_dioawait[3])))   # kb/s
+                ioawait = float_format(delta_rwuse / delta_rwio if delta_rwio else 0)   # 增量为0则设置为0
+
+                # store
+                self.disk_monitor_info[disk_name] = (disk_utilization,
+                                                     read_kb_ps, write_kb_ps,
+                                                     ioawait)
+            # store/update data
+            self.disk_info[disk_name] = new_info
 
     def get_qps_tps(self):
         # tps  total_commands_processed : <Sampling interval>
